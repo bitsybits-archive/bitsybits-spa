eb78f0e5ea275f433c5b6bc630b78c1dc7fde47d
Anton Sivakov <fanantoxa@gmail.com>
2018-02-10 00:37:49 +0200

    removed temporary solution

--- src/components/devices/device.vue
+++ src/components/devices/device.vue
@@ -9,20 +9,6 @@
         <button class="btn btn-primary" v-on:click="deleteDevice(device.hash)">Delete</button>
       </div>
   </div>
-  <!-- <div class="tile">
-    <div class="tile-icon">
-      <div class="example-tile-icon">
-        <i class="icon icon-file centered"></i>
-      </div>
-    </div>
-    <div class="tile-content">
-      <p class="tile-title">{{ device.name }}</p>
-      <p class="tile-subtitle text-gray">{{ device.hash }}</p>
-    </div>
-    <div class="tile-action">
-      <button class="btn btn-primary" v-on:click="deleteDevice(device.hash)">Delete</button>
-    </div>
-  </div> -->
 </template>
 <script>
   export default {
